2aa5094844e143fe574874bf5c99a383d96f7389
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-03 18:13:27 +0000

    Added show all character join for unique values.

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,36 +1,44 @@
 from database.connection import execute_query
+from pprint import pprint as ppshasta
 
-def create_new():
+print('The league of Superheroes would like to welcome you as an entry level sidekick.  First, you will need to add a record for yourself')
+
+# ***************CREATE A CHARACTER************
+def create_new_hero():
     query = """
         INSERT INTO heroes (name, about_me, biography)
-        VALUES (name_input, about_me_input, origin_input);
-        INSERT INTO ability_types(name)
-        VALUES (powers_input);
+        VALUES (%s, %s, %s);
     """
+    query2 ="""
+        INSERT INTO ability_types(name)
+        VALUES (%s);
+    """    
+    name_input = input('Enter superhero name: ')
+    about_me_input = input('Enter details about superhero: ')
+    origin_input = input('Enter superhero origin: ')
+    powers_input = input('Enter superhero powers: ')
 
-    name_input = input('The league of Superheroes would like to welcome you as an entry level sidekick.  First, you will need to add a record for yourself, what is your name?: ')
-    about_me_input = input('Tell us about you: ')
-    origin_input = input('What is your origin story?: ')
-    powers_input = input('What are your superpowers?: ')
+    execute_query(query, (name_input, about_me_input, origin_input))
+    execute_query(query2, (powers_input,))
 
-    create_you = execute_query(query, (name_input, about_me_input, origin_input, powers_input)).commit()
+# create_new_hero()
 
-create_new()
-
+# ***************SHOW ALL CHARACTERS**************
 def select_all():
     query = """
-        SELECT h.name, h.about_me, h.biography, at.name
-        FROM heroes h
-        JOIN ability_types at
-        ON (h.id = at.id)
-        JOIN abilities a ON
-        (at.id = a.id)
+        SELECT h.id, h.name, h.about_me, h.biography, STRING_AGG(abtype.name,',') as abilities
+                FROM heroes h
+                JOIN abilities abs
+                ON (h.id = abs.hero_id)
+                JOIN ability_types abtype ON
+                (abs.ability_type_id = abtype.id)
+                GROUP BY h.id
     """
     list_of_all = execute_query(query).fetchall()
-    print(list_of_all)
-    for record in list_of_all():
-        print(record[1])
+    ppshasta(list_of_all)
+select_all()
 
+# ***************SHOW ONE CHARACTERS**************
 def select_one():
     query = """
         SELECT *
