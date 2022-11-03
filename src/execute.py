1625fb36e3f22b9fffab28ce49ecdcadd415f9a7
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-03 23:28:41 +0000

    Added init function and if else statements to control actions

--- src/execute.py
+++ src/execute.py
@@ -9,6 +9,31 @@ from read import select_one
 from delete import another_one_bites_the_dust
 from update import add_ability_to_hero
 
-print('The league of Superheroes would like to welcome you as an entry level sidekick.  First, you will need to add a record for yourself')
+print('The league of Superheroes would like to welcome you as an entry level sidekick.  First, you will need to add a record for yourself.\n  NOTICE: Responses are continuously monitored and subject to auditing')
 
+# create_new_hero()
+# create_new_hero_ability_type()
+
+# Directory Function
+print("Great job sidekick, keep up the good work and you may just become a real superhero!")
+
+def directory():
+    print('----Actions----\n Enter the number of your selection \n(1) View the superhero roster\n(2) Add a new superhero\n(3) Update friends and enemies\n(4) Delete a superhero')
+
+directory()
+
+def init():
+    directory = input('Enter Number Selection From Directory: ')
+    if directory == '1':
+        print('You clicked one')
+    elif directory == '2':
+        print('You clicked two')
+    elif directory == '3':
+        print('You clicked three')        
+    elif directory == '4':
+        print('You clicked four')
+    else:
+        print('Please make a valid selection')
+
+init()
 # another_one_bites_the_dust()
